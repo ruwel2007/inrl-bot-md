ad6c4e42382f2999d16b1a0e4a2aa43dc330fa2c
inrl-official <fasweehmon2@gmail.com>
2023-07-19 10:34:48 +0530

    fix

---
+++ plugins/ping.js
@@ -0,0 +1,14 @@
+const {inrl} = require('../lib');
+
+inrl({
+    pattern: 'ping ?(.*)',
+    desc: 'To check ping',
+    sucReact: "💯",
+    category: ["system", "all"],
+    type: 'info'
+}, async (message, client) => {
+    const start = new Date().getTime()
+    const {key} = await message.reply('Ping!')
+    const end = new Date().getTime()
+    return await message.editMessage(message.from,'Pong! ' + (end - start) + ' ms', key);
+});
