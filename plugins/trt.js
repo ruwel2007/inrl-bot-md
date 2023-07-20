d691391d3a43d50addf87fd2cd5f376504e9452c
inrl-official <fasweehmon2@gmail.com>
2023-07-20 12:53:22 +0530

    smiply fix

--- plugins/trt.js
+++ plugins/trt.js
@@ -7,7 +7,7 @@ inrl(
 		type: 'search',
 	},
 	async (message, client, match) => {
-		if (!message.quoted && !message.quoted.text)
+		if (!message.reply_message.text)
 			return await message.send(
 				'*Reply to a text msg\n*_Example : trt ml_\ntrt en & ```reply to a text```'
 			)
