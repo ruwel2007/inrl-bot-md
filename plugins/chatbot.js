176d40188874cb29c6d06610545d3f2127e3d407
inrl-official <fasweehmon2@gmail.com>
2023-07-20 17:58:49 +0530

    Update chatbot.js

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -1,20 +1,16 @@
-const {getVar}=require('../lib');
-const got = require('got');
+const axios = require('axios');
 
-module.exports = async(msg, conn, m, store) => {
+module.exports = async(conn, m, vars) => {
   if((m.key.id.startsWith("BAE5") && m.key.id.length == 16)) return;
-  const data = await getVar();
-  const {PREFIX,AUTO_CHAT_PM,AUTO_CHAT_GRP} = data.data[0];
+  const {PREFIX,AUTO_CHAT_PM,AUTO_CHAT_GRP} = vars;
   const prefix = PREFIX=='false'?'':PREFIX;
   let isFalseOrNot = prefix==''?'i£':prefix;
   if(m.client.body.startsWith(isFalseOrNot)) return;
   if(AUTO_CHAT_PM == "true" && !m.isGroup){
-  let {body} = await got(`http://api.brainshop.ai/get?bid=172372&key=nbjE0YAlyw3cpoMl&uid=[${m.sender}]&msg=[${m.client.body}]`)
-  let value = JSON.parse(body).cnt;
-  return await m.reply(value)
+  let {data} = await axios(`http://api.brainshop.ai/get?bid=172372&key=nbjE0YAlyw3cpoMl&uid=[${m.sender}]&msg=[${m.client.body}]`)
+  return await m.reply(data.cnt)
   } else if(!m.client.body.startsWith(isFalseOrNot) && AUTO_CHAT_GRP == "true" && m.isGroup){
-  let {body} = await got(`http://api.brainshop.ai/get?bid=172372&key=nbjE0YAlyw3cpoMl&uid=[${m.sender}]&msg=[${m.client.body}]`)
-  let value = JSON.parse(body).cnt;
-  return await m.reply(value)
+  let {data} = await axios(`http://api.brainshop.ai/get?bid=172372&key=nbjE0YAlyw3cpoMl&uid=[${m.sender}]&msg=[${m.client.body}]`)
+  return await m.reply(data.cnt)
   }
 }
