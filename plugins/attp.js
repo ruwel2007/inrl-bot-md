0784de62d1c59711dbc453d07209af300fc2f73d
inrl-official <fasweehmon2@gmail.com>
2023-07-23 15:12:02 +0530

    test

--- plugins/attp.js
+++ plugins/attp.js
@@ -1,30 +1,32 @@
-const { inrl,getVar } = require('../lib');
-const axios = require("axios");
-
-inrl( { pattern: "ttp", sucReact: "🖼", category: ["all", "create"], type : "misc"},
-  async (message, client) => {
+const {
+    inrl,
+    getBuffer
+} = require('../lib');
+const {
+    BASE_URL
+} = require('../config');
+inrl({
+    pattern: "ttp",
+    type: "misc"
+}, async (message, client) => {
     if (!message.client.text) return await message.send("need text");
-    var uri = encodeURI(message.client.text);
-    try {
-      var resImage = await axios.get( "https://api.xteam.xyz/ttp?file&text=" + uri, { responseType: "arraybuffer" } );
-    } catch (error) {
-      return await message.send("error"+error);
-    }
-    let data = await getVar();
-    let {CAPTION}=data.data[0]
-    return await client.sendMessage( message.from, { image: Buffer.from(resImage.data), caption: CAPTION }, { quoted: message } );
-  }
-);
-
-inrl( { pattern: "attp", desc: "for attp img", sucReact: "😛", category: ["all", "create"], type : "misc"},
-  async (message, client) => {
+    const uri = encodeURI(message.client.text);
+    const res = `${BASE_URL}api/ttp?text=${uri}`
+    return await client.sendMessage(message.jid, {
+        image: {
+            url: res
+        }
+    });
+});
+inrl({
+    pattern: "attp",
+    type: "misc"
+}, async (message, client) => {
     if (!message.client.text) return await message.send("need text");
-    var uri = encodeURI(message.client.text);
-    try {
-      var resSticker = await axios.get( "https://api.xteam.xyz/attp?file&text=" + uri, { responseType: "arraybuffer" } );
-    } catch (error) { 
-        return message.send("error"+error);
-    }
-    return await client.sendMessage( message.from, { sticker: Buffer.from(resSticker.data) }, { quoted: message });
-  }
-);
+    const uri = encodeURI(message.client.text);
+    const res = await getBuffer(`${BASE_URL}api/attp?text=${uri}`);
+    return await client.sendFile(message.chat, res, "", message, {
+        asSticker: true,
+        categories: ["😑"],
+    });
+});
