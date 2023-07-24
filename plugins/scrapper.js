29c0def6a1a2d53aa283159df3818b00e4a99387
inrl-official <fasweehmon2@gmail.com>
2023-07-24 16:01:19 +0530

    Update scrapper.js

--- plugins/scrapper.js
+++ plugins/scrapper.js
@@ -1,23 +1,11 @@
 const {
-    getVar,
     inrl,
-    isUrl,
     googleIt,
-    wikiMedia,
-    ringTone,
-    getYtV,
-    getYtA,
     weather,
-    movie,
-    getFilm,
-    Insta,
-    mediafireDl,
-    twitter,
-    FaceBook,
-    wather
+    ringtone,
+    GenListMessage
 } = require('../lib');
-const Config = require('../config');
-const util = require('util');
+
 inrl({
     pattern: 'google',
     desc: 'do get goole serch result',
@@ -38,29 +26,6 @@ inrl({
     }
 });
 inrl({
-    pattern: 'wikimedia',
-    desc: 'do get data from wikimedia',
-    sucReact: "🙃",
-    category: ["system", "all"],
-    type: "search"
-}, async (message, client) => {
-    try {
-        if (!message.client.text) return message.send("need a text to serch");
-        let result = await wikiMedia(message.client.text);
-        let buttonMessage = {
-            image: {
-                url: result.image
-            },
-            caption: `Title : ${result.title}\n Source : ${result.source}\n Media Url : ${result.image}`,
-        }
-        return await client.sendMessage(message.from, buttonMessage, {
-            quoted: message
-        })
-    } catch (e) {
-        message.send(e)
-    }
-});
-inrl({
     pattern: 'ringtone',
     desc: 'do get random ringtons ',
     sucReact: "🙃",
@@ -69,20 +34,15 @@ inrl({
 }, async (message, client) => {
     try {
         if (!message.client.text) return message.send("need a text to serch");
-        let result = await ringTone(message.client.text);
-        return await client.sendMessage(message.from, {
-            audio: {
-                url: result.audio
-            },
-            fileName: result.title + '.mp3',
-            mimetype: 'audio/mpeg'
-        }, {
-            quoted: message
-        })
-    } catch (e) {
+        let result = await ringtone(message.client.text), res=[];
+        await result.map(r=>res.push(result.title));
+        return await client.sendMessage(message.jid, {
+            text: GenListMessage("AVAILABLE RINGTONES", res)
+            });
+     } catch (e) {
         message.send(e)
     }
-});
+});   
 
 inrl({
     pattern: 'weather',
@@ -92,8 +52,29 @@ inrl({
     type: "search"
 }, async (message, client, match) => {
     try {
-        return await wather(message, client);
+        return await weather(message, client);
     } catch (e) {
         return message.send(e);
     }
+});
+
+inrl({
+    pattern: 'dl-ringtone',
+    type: "downloader",
+    on: "text"
+}, async (m, conn, match) => {
+    if (!m.quoted || !m.quoted.fromMe) return;
+    if(!m.client.body.includes("AVAILABLE RINGTONES")) return;
+    match = m.client.body.replace("AVAILABLE RINGTONES", "").trim();
+    await m.send("*_downloading_*:-\n\n"+match);
+    let result = await ringtone(match);
+    return await conn.sendMessage(m.jid, {
+    audio:{
+                url: result[0].audio
+            },
+            fileName: result[0].title + '.mp3',
+            mimetype: 'audio/mpeg'
+        }, {
+            quoted: m
+     })
 });
