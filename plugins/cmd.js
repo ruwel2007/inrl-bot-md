011fa4aa55084173790f4233aba20f3763512026
inrl-official <fasweehmon2@gmail.com>
2023-07-25 00:18:11 +0530

    ficx

--- plugins/cmd.js
+++ plugins/cmd.js
@@ -13,7 +13,7 @@ inrl({
     type: "action",
     fromMe :true
 }, async (message, client, match, cmd) => {
-    if (!message.reply_message.sticker) return await message.reply("*_reply to a sticker msg and give me cmd name\nex: setcmd ping_*");
+    if (!message.reply_message.sticker) return await message.reply("*_reply to a sticker msg and give me cmd name_*\ne_x: setcmd ping_");
     if (!message.reply_message.msg.fileSha256) return message.send("_*Failed To Save Your Media Message!*_")
     if (!match) return await message.send("*_For Which Cmd!_*")
     await setCmd(message.quoted.msg.fileSha256.join(""), match)
@@ -41,7 +41,7 @@ inrl({
 }, async (message, client, match, cmd) => {
     let data = await getCmd(),
         cmds = "*Totel Cmds*";
-    if (data == "no data") return message.send('no sticker cmd in your db!')
+    if (data == "no data") return message.send('_*no sticker cmd in your db!*_')
     let n = 1;
     data.map((b) => {
         cmds += '```'+`${n++}  ${b.cmd}`+'```'+`\n`;
