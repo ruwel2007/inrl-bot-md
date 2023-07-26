8ebad7ea23b395d92dc0d7d98b0856972d5d60c2
inrl-official <fasweehmon2@gmail.com>
2023-07-27 00:56:12 +0530

    Update trt.js

--- plugins/trt.js
+++ plugins/trt.js
@@ -6,10 +6,10 @@ inrl(
 		desc: 'Google transalte',
 		type: 'search',
 	},
-	async (message, client, match) => {
+	async (message, match) => {
 		if (!message.reply_message.text)
 			return await message.send(
-				'*Reply to a text msg\n*_Example : trt ml_\ntrt en & ```reply to a text```'
+				'*Reply to a text msg*\n_Example : trt ml_\ntrt en & ```reply to a text```'
 			)
                 if(!match) return await message.send('*_giv me a language you want to convert_*');
                 const {text} = await TRT(message.quoted.text, match)
