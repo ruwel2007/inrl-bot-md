862442acd11d3d8fd8ad03948a4afbfd21b029a5
inrl-official <fasweehmon2@gmail.com>
2023-07-26 21:53:54 +0530

    test

--- plugins/download.js
+++ plugins/download.js
@@ -1,11 +1,11 @@
 const { inrl,extractUrlsFromString } = require('../lib');
 const {BASE_URL} = require('../config');
-const got = require('got');
+const axios = require('axios');
    inrl({
                 on: "text",
 		pattern: '$downloader',
 	},
-	async (message, client, match) => {
+	async (message, match) => {
 	const urls = extractUrlsFromString(message.client.body);
 	if(!urls[0]) return;
 	urls.map(async(url)=>{
@@ -21,19 +21,19 @@ const got = require('got');
 	}
 	}*/
 	if(url.match(/https?:\/\/(?:www\.)?instagram\.com(?:\/[^\/]+)?\/(?:p|reel)\/([^\/?#&]+)/)){
-	let {body} =  await got(BASE_URL + 'api/insta?url=' + url);
-	const {result} = JSON.parse(body);
+	let {data} =  await axios(BASE_URL + 'api/insta?url=' + url);
+	const {result} = data;
 	if(!result[0]) return await message.send('*Not Found*');
 	result.map(async(u)=> await message.sendFromUrl(u).catch((e)=>message.reply('*_request Filed With statusCode 503_*')))
 	}
 	if(url.match(/https:\/\/www\.mediafire\.com(?:\/[^\/]+)?\/(?:file)\/([^\/?#&]+)/)){
-	let {body} =  await got(BASE_URL + 'api/mediafire?url=' + url);
-	const {result} = JSON.parse(body);
+	let {data} =  await axios(BASE_URL + 'api/mediafire?url=' + url);
+	const {result} = data;
 	if(!result[0]) return await message.send('*Not Found *');
 	const {name,mime,size,link} = result[0]
 	await message.reply('```Name : ' + name.replaceAll('+',' ') + '\nSize : ' + size + '```\n\n_Downloading.._')
 	if(size>100) return await messsge.send('_fileLength is too high_')
-	await client.sendMessage(message.from, {
+	await message.client.sendMessage(message.from, {
 			document: {
 				url: link
 			},
