5513959114bc8d8d0003c088d5e9d0684ddb578c
inrl-official <fasweehmon2@gmail.com>
2023-07-27 00:39:19 +0530

    Update status.js

--- plugins/status.js
+++ plugins/status.js
@@ -7,11 +7,11 @@ const fs = require('fs');
 inrl({ pattern: 'send', DismissPrefix: true, desc: "it send wa status",sucReact: "⚒️",  category: ["owner"],type :"whatsapp"}, (async (message, match) => {
 if(!message.quoted.msg) return message.reply("*_reply to image/video!_*")
 if (message.quoted.videoMessage) {
-caption = message.quoted?.videoMessage?.captiontion;
+caption = message.quoted?.videoMessage?.caption;
 let location = await message.quoted.download();
 return await message.client.sendMessage(message.jid, { video: location, captiontion: caption });
 }else if (message.quoted.imageMessage) {
-caption = message.quoted?.imageMessage?.captiontion;
+caption = message.quoted?.imageMessage?.caption;
 let location = await message.quoted.download()
 return await message.client.sendMessage(message.jid, { image: location, captiontion: caption });
    } else if(message.quoted.audioMessage){
