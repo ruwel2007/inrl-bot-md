f4dc6b521b8aff15b7d6fd63b404d90d7be7004e
inrl-official <fasweehmon2@gmail.com>
2023-07-27 00:41:08 +0530

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,4 +1,4 @@
-const {inrl,getVar} = require("../lib/");
+const {inrl} = require("../lib/");
 const fs = require("fs");
 const path = require("path");
 
@@ -6,21 +6,19 @@ inrl(
   {
     pattern: "sticker",
     desc: "It cnvert image to sticker",
-    sucReact: "🔁",
-    category: ["all", "create"],
+    react: "🔁",
     type : 'converter',
     usage : "to convert short video or image to sticker fromate, ex:- sticker[repleyed_msg]"
   },
-  async (message, client) => {
-  let data = await getVar();
-  let {STICKER_DATA} = data.data[0];
+  async (message, match, data) => {
+  let {STICKER_DATA} = data;
     if (!/image|video|webp/.test(message.client.mime)) return await message.send(
           "Reply to Supported media With Caption"
         );
     try {
      if (message.quoted.mime) {
         let download = await message.quoted.download();
-        client.sendFile(message.from, download, "", message, {
+        message.client.sendFile(message.from, download, "", message, {
           asSticker: true,
           author: STICKER_DATA.split(';')[0],
           packname: STICKER_DATA.split(';')[1],
@@ -28,7 +26,7 @@ inrl(
         });
       } else if (/image|video|webp/.test(message.client.mime)) {
         let download = await client.downloadMediaMessage(message);
-        client.sendFile(message.from, download, "", message, {
+        message.client.sendFile(message.from, download, "", message, {
           asSticker: true,
           author: STICKER_DATA.split(';')[0],
           packname: STICKER_DATA.split(';')[1],
