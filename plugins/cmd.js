e56c24124fb49d62a3e1c13489b66f0f5b655240
inrl-official <fasweehmon2@gmail.com>
2023-07-28 06:26:47 +0530

    test

--- plugins/cmd.js
+++ plugins/cmd.js
@@ -8,38 +8,35 @@ const {
 inrl({
     pattern: 'setcmd',
     desc: 'To set media as a cmd',
-    sucReact: "😛",
-    category: ["system", "all"],
+    react: "😛",
     type: "action",
     fromMe :true
 }, async (message, match) => {
     if (!message.reply_message.sticker) return await message.reply("*_reply to a sticker msg and give me cmd name_*\ne_x: setcmd ping_");
     if (!message.reply_message.msg.fileSha256) return message.send("_*Failed To Save Your Media Message!*_")
     if (!match) return await message.send("*_For Which Cmd!_*")
-    await setCmd(message.quoted.msg.fileSha256.join(""), match)
+    await setCmd(message.quoted.msg.fileSha256.join(""), match,message.client.user.number)
     return await message.reply("_successfull_")
 });
 inrl({
     pattern: 'dltcmd',
     desc: 'To dlt media d as alrdy set',
-    sucReact: "💥",
-    category: ["system", "all"],
+    react: "💥",
     type: "action",
     fromMe :true
 }, async (message, match) => {
     if (!match) return await message.send("_*which cmd!*_")
-    await DeleteCmd(match)
+    await DeleteCmd(match,message.client.user.number)
     return await message.reply("_successfull_")
 });
 inrl({
     pattern: 'getcmd',
     desc: 'To dlt media d as alrdy set',
-    sucReact: "💥",
-    category: ["system", "all"],
+    react: "💥",
     type: "action",
     fromMe :true
 }, async (message, match) => {
-    let data = await getCmd(),
+    let data = await getCmd(message.client.user.number),
         cmds = "*Totel Cmds*";
     if (data == "no data") return message.send('_*no sticker cmd in your db!*_')
     let n = 1;
