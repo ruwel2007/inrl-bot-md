a572927b72202c272da13b6686db343aaf9b3d10
inrl-official <fasweehmon2@gmail.com>
2023-07-30 21:58:58 +0530

    test react

--- plugins/warn.js
+++ plugins/warn.js
@@ -1,4 +1,4 @@
-//created by @inrl
+>//created by @inrl
 
 const {
     inrl,
@@ -17,54 +17,45 @@ inrl({
     fromMe :true,
     onlyGroup :true
 }, async (message, match, data) => {
-if(!match && !message.quoted.sender) return await message.send("*_eg:-_*\n*warn* ```reply to a user```\n*warn* ```reset```\n*warn* ```list```");
-if(match && match == "get"){
+await message.send
+if(!match && !message.quoted.sender) return await message.send("*_eg:-_*\n*warn* ```reply to a user```\n*warn* ```reset```\n*warn* ```get```");
+if(match == "get"){
 let lists = await ListWarn(message.from, message.client.user.number), msg ="*List of Users*\n\n";
 if(!lists[0]) return await message.send('*_Not Found_*');
 lists.map((list)=>{
 msg += `*user:* ${list.user}\n*reason:* ${list.reason}\n*group:* ${list.group}@g.us\n\n`;
 });
 return await message.send(msg);
-} else if(match && match == "reset" && message.quoted.sender){
-        const u = message.quoted.sender;
-        const g = message.from;
-        const t = match.replace('reset','') || "reset";
-        const d = await ResetWarn(u, g, t, message.client.user.number)
+} else if(match == "reset"){
+if(!message.reply_message.sender) return await message.send('_reply to a user_');
+        const d = await ResetWarn(message.quoted.sender, message.from, message.client.user.number)
         return await message.send(`_successfull_`);
-        }
-        const BotAdmin = await isBotAdmin(message, message.client);
-        const admin = await isAdmin(message, message.client);
+        } else {
+        const BotAdmin = await isBotAdmin(message);
+        const admin = await isAdmin(message);
         if (!BotAdmin) return await message.reply('*_Bot must Be Admin_*');
-    if (!admin && !message.client.isCreator) return await message.send('*_request failed with statuscode 403*_');
+    if (!admin && !message.client.isCreator) return await message.send('*_request failed with statuscode 403_*');
     let {
         WARNCOUND
     } = data
-    if (message.quoted.sender) {
-        const u = message.quoted.sender;
-        const g = message.from;
+    if(!message.reply_message.sender) return await message.send('_reply to a user_');
         const t = match || "warning";
-        const d = await setWarn(u, g, t,message.client.user.number)
-        let count = d.count,
-            COUND = WARNCOUND;
-        let remains = COUND - count;
+        const d = await setWarn(message.quoted.sender, message.from, t,message.client.user.number)
+        let remains = WARNCOUND - d.count;
         let warnmsg = `❏─────[ ᴡᴀʀɴɪɴɢ ]─────❏
-│ Group:-${d.group}
 │ User :-${d.user}
 ❏───────────────────❏
 ┏─────── INFO ───────❏
 │ Reason :- ${d.reason}
 │ Count :- ${d.count}
 │ Remaining :- ${remains}
-┗•──────────────────❏`
+┗•─────────────────❏`
         await message.reply(warnmsg)
-        if (remains <= "0") {
-            const u = message.quoted.sender;
-            const g = message.from;
-            const t = match || "reset";
-            const d = await ResetWarn(u, g, t, message.client.user.number)
+        if (remains <= 0) {
+            const d = await ResetWarn(message.reply_message.sender, message.from,  message.client.user.number)
             if(BotAdmin){
             await message.client.groupParticipantsUpdate(message.from, [message.quoted.sender], "remove");
-            return await message.reply("_user removes from the group due to warn limit existence_")
+            return await message.reply("_Max warns reached, kicked out!_")
             };
         };
     };
